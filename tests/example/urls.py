28c2e495df97e61c09b50ae901a0619cd685e062
Sam <samuel.scott.jennings@gmail.com>
2024-08-28 16:53:51 +0200

    update example app

--- tests/example/urls.py
+++ tests/example/urls.py
@@ -1,10 +1,19 @@
-from django.urls import path, include
-from django.views.generic import TemplateView
+from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.urls import include, path
+from django.views.generic.edit import UpdateView
+
+# from debug_toolbar import urls as debug_toolbar
+from .views import Home
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="account_management/base.html"), name="home"),
-    path('accounts/', include('allauth.urls')),
-    path('admin/', admin.site.urls),
-]
-
+    path("", Home.as_view(), name="home"),
+    path(
+        "profile/<pk>/",
+        UpdateView.as_view(model=get_user_model(), fields=["username", "first_name", "last_name"]),
+        name="profile-edit",
+    ),
+    path("accounts/", include("allauth.urls")),
+    path("admin/", admin.site.urls),
+] + debug_toolbar_urls()
